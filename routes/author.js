b353fa9ca560bdf509bede47e3dbfdaa4bb1135b
Mostafa Ahmed <79556633+mosta1489@users.noreply.github.com>
2022-03-25 08:27:12 +0200

    edit post method to create author

--- routes/author.js
+++ routes/author.js
@@ -11,21 +11,20 @@ router.get("/new", (req, res) => {
 });
 
 // Create Authors Route
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const author = new Author({
     name: req.body.name,
   });
-  author.save((err, newAuthor) => {
-    if (err) {
-      res.render("authors/new", {
-        author: author,
-        errorMassage: "Error creating Author",
-      });
-    } else {
-      // res.redirect(`authors/${newAuthor.id}`);
-      res.redirect(`authors`);
-    }
-  });
+  try {
+    const newAuthor = await author.save();
+    // res.redirect(`authors/${newAuthor.id}`);
+    res.redirect(`authors`);
+  } catch {
+    res.render("authors/new", {
+      author: author,
+      errorMassage: "Error creating Author",
+    });
+  }
 });
 
 module.exports = router;
